785bb12b664ee53ebf3bdf3e873bf3d01c5062b5
Bárbara Barone - BBA <barbara.barone@iteris.com.br>
2020-04-15 13:05:25 -0300

    Initial Commit

---
+++ FB2019/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FB2019
+//
+//  Created by Bárbara Barone - BBA on 15/04/20.
+//  Copyright © 2020 Bárbara Barone - BBA. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
